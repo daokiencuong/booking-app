6efa27da33457a6a6eb1000e26dae5fd961efa40
daokiencuong <daokiencuong04@gmail.com>
2025-07-07 21:32:44 +0700

    Add demo 2 features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'auth', loadComponent: () => import('./features/auth/auth').then(m => m.Auth) },
+    { path: 'booking', loadComponent: () => import('./features/booking/booking').then(m => m.Booking) },
+];
