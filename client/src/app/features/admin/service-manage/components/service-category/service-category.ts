87ffcb23614b287891593c562c22f293ef109205
daokiencuong <daokiencuong04@gmail.com>
2025-08-10 16:41:47 +0700

    Create table in category to show the mainservice

--- client/src/app/features/admin/service-manage/components/service-category/service-category.ts
+++ client/src/app/features/admin/service-manage/components/service-category/service-category.ts
@@ -1,10 +1,11 @@
 import { Component, input, signal } from '@angular/core';
 import { ServiceCategoryGet } from '../../../../../model/response/service/service-category-get.model';
 import { MainService } from '../main-service/main-service';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-service-category',
-  imports: [MainService],
+  imports: [MainService, DatePipe, CommonModule],
   templateUrl: './service-category.html',
   styleUrl: './service-category.css'
 })
